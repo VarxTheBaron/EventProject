9e3c5c72d6858851badc4117e76f8ba8abf8f48d
Jonas Persson <jonas.persson111@gmail.com>
2026-02-09 15:37:01 +0100

    -

--- src/EventHomepage/Controllers/Api/EventsController.cs
+++ src/EventHomepage/Controllers/Api/EventsController.cs
@@ -2,6 +2,7 @@ using EventCore.Entities;
 using EventInfrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Primitives;
 using SQLitePCL;
 
 namespace EventHomepage.Controllers.Api;
@@ -10,9 +11,23 @@ namespace EventHomepage.Controllers.Api;
 [Route("/api/v1/[controller]")]
 public class EventsController(EventDbContext _db) : ControllerBase
 {
+    private string _correctPw = "ThinkpadsOverMac";
+
     [HttpPost]
     public async Task<IActionResult> CreateEvent(EventDTO dto)
     {
+        StringValues pw;
+
+        if (!Request.Headers.TryGetValue("password", out pw))
+        {
+            return Unauthorized();
+        }
+
+        if (pw != _correctPw)
+        {
+            return Unauthorized();
+        }
+
         try
         {
             Event newEvent = new Event(
@@ -34,6 +49,17 @@ public class EventsController(EventDbContext _db) : ControllerBase
     [HttpDelete("{id}")]
     public async Task<IActionResult> DeleteEvent(int id)
     {
+        StringValues pw;
+
+        if (!Request.Headers.TryGetValue("password", out pw))
+        {
+            return Unauthorized();
+        }
+
+        if (pw != _correctPw)
+        {
+            return Unauthorized();
+        }
 
         if (await _db.Events.Where(e => e.Id == id).ExecuteDeleteAsync() == 0)
             return NotFound();
@@ -47,6 +73,18 @@ public class EventsController(EventDbContext _db) : ControllerBase
     [HttpPut("{id}")]
     public async Task<IActionResult> UpdateEvent(int id, EventDTO dto)
     {
+        StringValues pw;
+
+        if (!Request.Headers.TryGetValue("password", out pw))
+        {
+            return Unauthorized();
+        }
+
+        if (pw != _correctPw)
+        {
+            return Unauthorized();
+        }
+
         var _event = await _db.Events.FindAsync(id);
 
         if (_event == null) return NotFound();
